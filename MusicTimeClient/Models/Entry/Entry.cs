7a9e210d0ac0ba77576a0a1a5b007cf54658f346
montify <montify@gmail.com>
2024-12-08 23:46:36 +0100

    Cleanup

--- MusicTimeClient/Models/Entry/Entry.cs
+++ MusicTimeClient/Models/Entry/Entry.cs
@@ -2,21 +2,20 @@
 {
     public class Entry
     {
-        public Entry()
-        {
-            
-        }
-        public Entry(string description, int durationInMinutes)
-        {
-            Description = description;
-            DurationInMinutes = durationInMinutes;
-        }
-
         public int Id { get; set; }
         public DateTime Date { get; set; } = DateTime.Now.Date;
         public int DurationInMinutes { get; set; }
         public bool IsFinished { get; set; }
         public string Description { get; set; }
         public string DateToString() => Date.ToString("dd/MM/yy/").Trim('.');
+
+        public Entry()
+        {
+        }
+        public Entry(string description, int durationInMinutes)
+        {
+            Description = description;
+            DurationInMinutes = durationInMinutes;
+        }
     }
 }
