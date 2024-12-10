881252f2dcadef9434e7ced89e1501aef567c0a7
montify <montify@gmail.com>
2024-12-10 23:32:50 +0100

    Add AuthHandler

---
+++ MusicTimeClient/AuthHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Headers;
+
+namespace MusicTimeClient
+{
+    public class AuthHandler : DelegatingHandler
+    {
+        private readonly LocalStorage m_localStorage;
+
+        public AuthHandler(LocalStorage localStorage)
+        {
+            m_localStorage = localStorage;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+           
+            var token = await m_localStorage.GetItem("jwtToken");
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+           
+            var send = await base.SendAsync(request, cancellationToken);
+            return send; 
+        }
+    }
+}
